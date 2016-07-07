58aae3f0b1d2020ad1808a1e99bfbd1b865f53c2
kimballfrank <kimballfrank@hotmail.com>
2016-07-07 15:54:20 -0600

    increase maxBuffer

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ var path = require('path')
 var child_process = require('child_process')
 
 module.exports = function (args, callback) {
-	args.maxBuffer = 1024 * 1000;
-  child_process.exec(module.exports.binaryPath + ' ' + args, callback)
+  child_process.exec(module.exports.binaryPath + ' ' + args, {maxBuffer: 1024 * 1000}, callback);
 }
 
 module.exports.binaryPath = path.join(__dirname, 'sketchtool', 'bin', 'sketchtool')
